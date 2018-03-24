1e75eb909541516a234929bed9c3c9d12a0ba25a
Juli Cheng <94julicheng@gmail.com>
2018-03-24 22:28:33 +0000

    add init function and restartGame function

--- js/app.js
+++ js/app.js
@@ -18,12 +18,19 @@ let cardSymbols = [
   "fa-bomb"
 ];
 
-// Select all cards
+// Initialize variables
 let cards = document.querySelectorAll(".card");
-
+let movesDisplay = document.querySelector(".moves");
+let restart = document.querySelector(".fa-repeat");
 let openCards = [];
 let moves = 0;
 let matches = 0;
+
+function init() {
+    updateCards();
+    addEventListeners();
+}
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -44,24 +51,15 @@ function shuffle(array) {
 function updateCards() {
     shuffle(cardSymbols);
     for (let i = 0; i < cards.length; i++) {
+        cards[i].classList = "card";
         cards[i].firstElementChild.classList = "";
         cards[i].firstElementChild.classList+= cardSymbols[i] + " fa";
     }
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
 // Set up event listener for a card when it is clicked
 function addEventListeners() {
+    restart.addEventListener("click", restartGame);
     for (let i = 0; i < cards.length; i++) {
         // Display the card's symbol
         cards[i].addEventListener("click", showSymbol);
@@ -117,19 +115,33 @@ function removeOpenCards() {
     openCards = [];
 }
 
-function checkMatches() {
-    matches++;
-    if (matches === 8) {
-      console.log("you have won!");
-    }
-}
-
 // Count moves made
 function addMoves() {
     moves++;
+    movesDisplay.innerHTML = moves;
     console.log(moves);
 }
 
+// Check matches
+function checkMatches() {
+    matches++;
+    if (matches === 8) {
+      winnersScreen();
+    }
+}
+
+function winnersScreen() {
+    console.log("winner!")
+}
+
+function restartGame() {
+    openCards = [];
+    moves = 0;
+    movesDisplay.innerHTML = 0;
+    matches = 0;
+
+    updateCards();
+}
+
 // when the page loads
-updateCards();
-addEventListeners();
+init();
